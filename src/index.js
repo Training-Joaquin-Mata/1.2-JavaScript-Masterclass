24c3425542d163d5d76cea6937a1b912b35325cc
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-21 14:12:02 -0500

    Immutable vs Mutable Concepts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import '../assets/css/style.css';
-
+/*
 const items = [
   { id: '🍔', name: 'Super Burger', price: 399 },
   { id: '🍟', name: 'Jumbo Fries', price: 199 },
   { id: '🥤', name: 'Big Slurp', price: 299 },
 ];
 
-console.log(items);
+console.log(items); */
+
+
+let a = 'Super Burguers';
+a='Big Slurp';
+
+const b = a.slice(0,1);
+console.log(a,b);
+
+const x=Object.freeze( {id: '🍟', name: 'Jumbo Fries', price: 199});
+
+console.log(x);
