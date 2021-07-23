f57e8c8bd5c90d7cfee86bd64525dd5108668187
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-23 10:29:54 -0500

    Object Literals and Factory Functions

--- src/index.js
+++ src/index.js
@@ -24,22 +24,44 @@ import '../assets/css/style.css';
 // }, 2500);
 
 // <--------------------------------------------------- Object Literals and Encapsulation
-const cart ={
-  items:[],
+// const cart ={
+//   items:[],
+//   add(item){
+//     this.items.push(item);
+//     },
+//   remove(id){
+//       const index = this.items.findIndex((x) => x.id===id);
+//       this.items.splice(index, 1);
+//     },
+  
+// };
+
+// const hotDog = {id:'🌭', name:'Posh Dog', price: 399};
+
+// cart.add(hotDog);
+// console.log(cart);
+
+
+// setTimeout(()=>{
+//   cart.remove('🌭');
+//   console.log(cart);
+// }, 2500);
+
+// <--------------------------------------------------- Object Literals and Factory Functions
+
+const createCart=(items=[])=>({
+  
+    items,
   add(item){
     this.items.push(item);
     },
   remove(id){
       const index = this.items.findIndex((x) => x.id===id);
       this.items.splice(index, 1);
-    },
-  
-};
+    }
+});
 
-
-
-
- 
+const cart= createCart([]);
 
 const hotDog = {id:'🌭', name:'Posh Dog', price: 399};
 
