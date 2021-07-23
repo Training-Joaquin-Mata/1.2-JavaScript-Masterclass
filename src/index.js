28b8573d1afd6fa7d3c66f48084f9bd97730afdb
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-23 09:58:35 -0500

    Procedural Programming

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import '../assets/css/style.css';
 
-const items = [
-  { id: '🍔', name: 'Super Burger', price: 399 },
-  { id: '🍟', name: 'Jumbo Fries', price: 199 },
-  { id: '🥤', name: 'Big Slurp', price: 299 },
-];
 
-console.log(items);
+const cart =[];
+
+const addToCart =(item)=> cart.push(item);
+
+
+
+const removeFromCart = (id) =>  {
+  const index = cart.findIndex((x) => x.id===id);
+  cart.splice(index, 1);
+}; 
+
+const hotDog = {id:'🌭', name:'Posh Dog', price: 399};
+
+addToCart(hotDog);
+console.log(cart);
+
+
+setTimeout(()=>{
+  removeFromCart('🌭');
+  console.log(cart);
+}, 2500);
+
+
+
