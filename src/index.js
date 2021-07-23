457362cc1546eccc889a1566ef9fcea3c1b6b698
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-23 10:18:22 -0500

    Object Literals and Encapsulation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,53 @@
 import '../assets/css/style.css';
 
+// <--------------------------------------------------- Procedural Programming
+// const cart =[];
 
-const cart =[];
-
-const addToCart =(item)=> cart.push(item);
+// const addToCart =(item)=> cart.push(item);
 
 
 
-const removeFromCart = (id) =>  {
-  const index = cart.findIndex((x) => x.id===id);
-  cart.splice(index, 1);
-}; 
+// const removeFromCart = (id) =>  {
+//   const index = cart.findIndex((x) => x.id===id);
+//   cart.splice(index, 1);
+// }; 
+
+// const hotDog = {id:'🌭', name:'Posh Dog', price: 399};
+
+// addToCart(hotDog);
+// console.log(cart);
+
+
+// setTimeout(()=>{
+//   removeFromCart('🌭');
+//   console.log(cart);
+// }, 2500);
+
+// <--------------------------------------------------- Object Literals and Encapsulation
+const cart ={
+  items:[],
+  add(item){
+    this.items.push(item);
+    },
+  remove(id){
+      const index = this.items.findIndex((x) => x.id===id);
+      this.items.splice(index, 1);
+    },
+  
+};
+
+
+
+
+ 
 
 const hotDog = {id:'🌭', name:'Posh Dog', price: 399};
 
-addToCart(hotDog);
+cart.add(hotDog);
 console.log(cart);
 
 
 setTimeout(()=>{
-  removeFromCart('🌭');
+  cart.remove('🌭');
   console.log(cart);
 }, 2500);
-
-
-
